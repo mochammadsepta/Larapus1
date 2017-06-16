dfb6562c0f3039fff74475d3d9d4da57b1aa7bd2
Mochammad Septa <Mochammad Septa>
2017-06-16 21:14:12 +0700

    5.3

--- app/Http/Requests/UpdateBookRequest.php
+++ app/Http/Requests/UpdateBookRequest.php
@@ -18,7 +18,7 @@ class UpdateBookRequest extends StoreBookRequest
     public function rules()
     {
         $rules = parent::rules();
-        $rules['title']='required|unique:book,title,'.$this->route('book');
+        $rules['title']='required|unique:books,title,'.$this->route('book');
         return $rules;
     }
 }
